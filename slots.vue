36a2b106298ed90b3a15566a4c915a235d9ae1b0
Nematjon <frank.sport.93@gmail.com>
2020-11-27 10:58:23 +0300

    slot component explained

---
+++ slots.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <header>
+      <!-- THis is named slot -->
+      <slot name="header"></slot>
+    </header>
+    <!--
+      Now we can use syntax to reach the slot
+        <template v-slot:header>
+          <h1>Here might be a page title</h1>
+        </template>
+    -->
+    <main>
+      <!-- This is default slot, it can omit the name attribute -->
+      <slot></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SlotComponent"
+  };
+</script>
