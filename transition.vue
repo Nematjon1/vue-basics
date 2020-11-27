935f0951e142c546434a5f947b08efbb061c96bd
Nematjon <frank.sport.93@gmail.com>
2020-11-27 12:17:30 +0300

    added animation effect.

--- transition.vue
+++ transition.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="example-1">
+    <!-- Transition effect -->
     <button v-on:click="show = !show">
       Toggle render
     </button>
     <transition name="slide-fade">
       <p v-if="show">hello</p>
+    </transition>
+    
+    <!-- Animation effect -->
+    <button @click="show = !show">Toggle show</button>
+    <transition name="bounce">
+      <p v-if="show">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis enim libero, at lacinia diam fermentum id. Pellentesque habitant morbi tristique senectus et netus.</p>
     </transition>
   </div>
 </template>
@@ -33,5 +40,28 @@ export default {
 /* .slide-fade-leave-active below version 2.1.8 */ {
   transform: translateX(10px);
   opacity: 0;
+}
+
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+
+/**
+  CSS animations are applied in the same way as CSS transitions,
+  the difference being that v-enter is not removed immediately after the element is inserted, but on an "animationend" event.
+**/
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
