901daf876ec0260bd1a9b2dca2f2bbf96a09f324
Nematjon <frank.sport.93@gmail.com>
2020-11-27 12:13:21 +0300

    transition implementation.

---
+++ transition.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="example-1">
+    <button v-on:click="show = !show">
+      Toggle render
+    </button>
+    <transition name="slide-fade">
+      <p v-if="show">hello</p>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TransitionComponent",
+  data() {
+    return {
+      show: true
+    };
+  }
+};
+</script>
+
+<style>
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
